ce45ce7e5557ac88d93f6d53e1e5230bf2eed510
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-04-06 12:01:09 +0300

    add new controllers

---
+++ ssmpb/FourZeroFour.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\PageBuilder;
+
+class FourZeroFour extends PageBuilder
+{
+    
+    protected $template = '404';
+    protected $acf = false;
+
+}
---
+++ ssmpb/Taxonomy.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\PageBuilder;
+
+class Taxonomy extends PageBuilder
+{
+
+}
